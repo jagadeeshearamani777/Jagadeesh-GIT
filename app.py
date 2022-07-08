0ff30306f0ac869d9a33bceeee7a1857c643c2fd
jagadeeshearamani777 <jagadeeshearamani@gmail.com>
2022-07-08 15:25:14 +0530

    kalyani files

---
+++ app.py
@@ -0,0 +1,14 @@
+class Calculator:
+
+    def __init__(self,a=0,b=20) -> None:
+        self.a=a
+        self.b=b
+
+    def addition(self) -> int:
+        return self.a+self.b
+
+    def subtraction(self) -> float:
+        return self.a-self.b
+
+cal=Calculator(a=3,b=4)
+print(f"Adding the values:-  {cal.addition()}")
